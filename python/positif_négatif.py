2d486e1037fd61e42252a9c9011769d0e52f38b0
yahya-2005 <yahyalaadam3@gmail.com>
2024-11-02 10:22:46 +0100

    Update positif_négatif.py

---
+++ python/positif_négatif.py
@@ -0,0 +1,7 @@
+nombre = float(input("Entrez un nombre : "))
+if nombre > 0:
+    print(nombre, "est positif")
+elif nombre < 0:
+    print(nombre, "est négatif")
+else:
+    print("Le nombre est nul")
